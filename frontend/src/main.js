8676ba94b6fe20154ffb38cbaef8575ee6348846
신현수 <75037683+hyunsooshin04@users.noreply.github.com>
2021-12-28 14:26:07 +0900

    project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,4 +9,5 @@ axios.defaults.baseURL = 'http://locallhost:3000'
 // createApp(App).use(store).use(router).mount("#app");
 const app = createApp(App)
 app.config.globalProperties.axios = axios;
+
 app.use(store).use(router).mount('#app')
